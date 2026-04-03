940fadd2a52bba7e1323bbf4362c2986c56618ec
Delia-1 <delia.knoepfli@gmail.com>
2026-04-03 10:29:03 +0200

    render about banner on about page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,31 @@
+import { useEffect } from "react";
 import banner from "../assets/banner.svg";
+import bannerAbout from "../assets/bannerAbout.svg";
+import { useLocation } from "react-router-dom";
+
 const Banner = () => {
+let location = useLocation()
+
+useEffect(() => {
+  const banner1 = new Image()
+  banner1.src = banner;
+  const banner2 = new Image()
+  banner2.src = bannerAbout
+
+}, [location.pathname])
+const isAboutPage = location.pathname === "/about"
+
   return (
-    <div className="banner">
-      <img className="banner__image" src={banner} alt="banner image" />
+    <div className={!isAboutPage ? "banner" : "bannerAbout"}>
+      {!isAboutPage ? 
+      (<>
+     <img className="banner__image" src={banner} alt="banner image" />
       <p className="banner__title">
         Chez vous, <br /> partout et ailleurs
       </p>
+     </>)
+      : 
+      ( <img className="banner__image" src={bannerAbout} alt="banner image" />)}
     </div>
   );
 };
