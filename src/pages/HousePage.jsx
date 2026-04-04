164a0648a366b83c6eef739bb84c50a6bc7127da
Delia-1 <delia.knoepfli@gmail.com>
2026-04-04 11:29:31 +0200

    init implem housepage

--- src/pages/HousePage.jsx
+++ src/pages/HousePage.jsx
@@ -1,5 +1,11 @@
 import { useParams } from "react-router-dom";
 import mockedData from "../data/mockedData.json";
+import Slideshow from "../components/housepage/Slideshow";
+import Collapse from "../components/Collapse";
+import Tag from "../components/housepage/Tag";
+import Rating from "../components/housepage/Rating";
+
+
 
 const HousePage = () => {
   const { housepageId } = useParams();
@@ -8,13 +14,28 @@ const HousePage = () => {
     return item.id === housepageId;
   });
 
+const {title, description, pictures, tags, rating} = data
+
   return (
     <>
-      <div>
-        <p>id: {housepageId}</p>
-        <p>{data.title}</p>
-        <p>{data.description}</p>
-        <img src={data.pictures[1]} alt="" />
+      <div className="housepage">
+        <Slideshow  pictures={pictures} />
+        <h2 className="housepage__title">{title}</h2>
+        <p className="housepage__desc">{description}</p>
+        <div className="housepage__tags">
+          {tags.map((tag, index) => (    
+            <Tag key={`${tag}-${index}`} tag={tag}/>
+          ))}
+        </div>
+        <div>
+          <Rating nbStars={rating}/>
+          <div>
+            <p></p>
+            {/* <img src="" alt="" /> */}
+          </div>
+        </div>
+        <Collapse />
+        <Collapse />
       </div>
     </>
   );
