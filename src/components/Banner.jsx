5d4f10837f713f53e2c1d3955ba21438b7c08afe
Delia-1 <delia.knoepfli@gmail.com>
2026-04-19 08:31:05 +0200

    Refacto banner: simplified with scss bg image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,32 +1,19 @@
-import { useEffect } from "react";
-import banner from "../assets/banner.svg";
-import bannerAbout from "../assets/bannerAbout.svg";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import srcBanner from "../assets/banner.svg";
+import srcBannerAbout from "../assets/bannerAbout.svg";
 
 const Banner = () => {
-let location = useLocation()
-
-// utiliser lazy ou suspens pour la banner
-useEffect(() => {
-  const banner1 = new Image()
-  banner1.src = banner;
-  const banner2 = new Image()
-  banner2.src = bannerAbout
-
-}, [location.pathname])
-const isAboutPage = location.pathname === "/about"
+  const location = useLocation();
+  const isAboutPage = location.pathname === "/about";
 
   return (
-    <div className={!isAboutPage ? "banner" : "bannerAbout"}>
-      {!isAboutPage ? 
-      (<>
-     <img className="banner__image" src={banner} alt="banner image" />
-      <p className="banner__title">
-        Chez vous, <br /> partout et ailleurs
-      </p>
-     </>)
-      : 
-      ( <img className="banner__image" src={bannerAbout} alt="banner image" />)}
+    <div className={isAboutPage ? "bannerAbout" : "banner"}>
+      {!isAboutPage ? (
+        <p className="banner__title">
+          Chez vous, <br /> partout et ailleurs
+        </p>
+      ) : null}
     </div>
   );
 };
